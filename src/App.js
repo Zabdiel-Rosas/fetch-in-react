79e61e3fc83ad6236abeb9a6c2373898e99bf129
Zabdiel Rosas <zabdiel_rosas@outlook.com>
2023-03-12 17:50:44 -0500

    feat(useFetch, App): error handling functionality is added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { useFetch } from './useFetch'
 import './App.css'
 
 function App() {
-  const { data, loading } = useFetch('https://jsonplaceholder.typicode.com/users')
+  const { data, loading, error } = useFetch(
+    'https://jsonplaceholder.typicode.com/users'
+  )
+
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>Fetch Like a Pro</h1>
         <div className='card'>
           <ul>
+            {error && <li>Error: {error}</li>}
             {loading && <li>Loading...</li>}
             {data?.map((user) => (
               <li key={user.id}>{user.name}</li>
